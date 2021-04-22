a0240afd37b19c430bab12af7f6c528d73583634
Fede Lizondo <fedealiza@gmail.com>
2021-04-22 20:02:19 -0300

    test routes

--- src/resources/js/components/pages/main/MainComponent.vue
+++ src/resources/js/components/pages/main/MainComponent.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h2>Listado para Proyectos</h2>
-    <h2>Listados para favoritos</h2>
+    <ul>
+      <li><router-link to="/proyecto/1"> Ir a proyecto 1</router-link></li>
+    </ul>
+    <h2>Listados para Favoritos</h2>
   </div>
 </template>
 
