8c6ccb8b0979c7e2f70ea90f9d25b0168b34154c
Fede Lizondo <fedealiza@gmail.com>
2021-04-22 19:07:41 -0300

    test routes

--- src/resources/js/components/pages/search/ProyectoRowComponent.vue
+++ src/resources/js/components/pages/search/ProyectoRowComponent.vue
@@ -1,25 +1,27 @@
 <template>
   <div id="container">
     <div id="row" class="shadow-sm row rounded p-2">
-      <div
-        class="col-10 col-lg-11 d-flex justify-content-start align-items-center"
-      >
-        <div id="nombre" class="d-inline-block text-wrap p-2">
-          <div style="font-size: 1.1rem">
-            {{ item.nombre }}
+      <router-link :to="{ name: 'Proyecto', params: { id: item.id } }">
+        <div
+          class="col-10 col-lg-11 d-flex justify-content-start align-items-center"
+        >
+          <div id="nombre" class="d-inline-block text-wrap p-2">
+            <div style="font-size: 1.1rem">
+              {{ item.nombre }}
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        class="col-2 col-lg-1 d-flex flex-row justify-content-around justify-content-md-between text-truncate align-items-center"
-        @click="item.fav = item.fav ? false : true"
-      >
-        <font-awesome-icon
-          :icon="item.fav ? ['fas', 'star'] : ['far', 'star']"
-          :style="{ color: '#EBCB29' }"
-          size="lg"
-        />
-      </div>
+        <div
+          class="col-2 col-lg-1 d-flex flex-row justify-content-around justify-content-md-between text-truncate align-items-center"
+          @click="item.fav = item.fav ? false : true"
+        >
+          <font-awesome-icon
+            :icon="item.fav ? ['fas', 'star'] : ['far', 'star']"
+            :style="{ color: '#EBCB29' }"
+            size="lg"
+          />
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
