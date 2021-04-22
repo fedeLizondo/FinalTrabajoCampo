10e7af75f6698b6d26cce50b5688914f0714f8c3
Fede Lizondo <fedealiza@gmail.com>
2021-04-22 18:58:17 -0300

    test routes

--- src/resources/js/routes.js
+++ src/resources/js/routes.js
@@ -5,5 +5,5 @@ import Proyecto from './components/pages/project/ProyectoComponent.vue'
 export const routes = [
     { path: '/vue/:id', component: Proyecto, name: 'Home' },
     { path: '/vue/example', component: Example, name: 'Example' },
-    { path: '/vue/proyecto/:id', componente: Proyecto, name: 'Proyecto', params: true}
+    { path: '/vue/proyecto/:id', component: Proyecto, name: 'Proyecto', params: true}
 ];
