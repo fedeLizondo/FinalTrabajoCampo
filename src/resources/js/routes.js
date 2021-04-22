24a29406843e9c77324732ef8e53825a9240134e
Fede Lizondo <fedealiza@gmail.com>
2021-04-22 19:10:55 -0300

    test routes

--- src/resources/js/routes.js
+++ src/resources/js/routes.js
@@ -1,9 +1,11 @@
 import Home from './components/Home.vue';
 import Example from './components/Example.vue';
 import Proyecto from './components/pages/project/ProyectoComponent.vue'
+import Search from './components/pages/search/SearchComponent.vue'
 
 export const routes = [
     { path: '/vue/:id', component: Proyecto, name: 'Home' },
     { path: '/vue/example', component: Example, name: 'Example' },
-    { path: '/vue/proyecto/:id', component: Proyecto, name: 'Proyecto', params: true}
+    { path: '/vue/proyecto/:id', component: Proyecto, name: 'Proyecto', params: true },
+    { path: '/vue/search', component: Search, name: 'Search', params: true },
 ];
