c8943fe06ce0475b03fa6da45ca185f73c2ddd92
Fede Lizondo <fedealiza@gmail.com>
2021-04-22 01:25:16 -0300

    fix routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,5 +21,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/vue/{vue_capture?}', function () {
-    return view('vue.index');
+    return view('home');
    })->where('vue_capture', '[\/\w\.-]*');
