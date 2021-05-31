c1e39ee8904045b42ab4a089a8d0fb7d6157cca7
Fede Lizondo <fedealiza@gmail.com>
2021-05-31 02:56:19 -0300

    make refactor and cu base

---
+++ src/resources/js/components/pages/project/Diagrama/DiagramaComponent.vue
@@ -0,0 +1,117 @@
+<template>
+  <modal-component ref="modalInternoDiagrama">
+    <template v-slot:header>
+      <h3>{{ !isUpdate ? "Agregar " : "Modificar " }} diagrama</h3>
+    </template>
+
+    <template v-slot:body>
+      <div class="form-group">
+        <label for="selectDiagrama">Diagrama </label>
+        <select
+          class="form-control"
+          id="selectDiagrama"
+          v-model="diagrama.type"
+          required
+          :disabled="isUpdate"
+        >
+          <option value="CLASE">Clase</option>
+          <option value="CU">Caso de Uso</option>
+          <option value="SECUENCIA">Secuencia</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="nombre">Nombre de diagrama:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="nombre"
+          v-model="diagrama.nombre"
+        />
+      </div>
+      <div class="form-group">
+        <label for="descripcion">Descripción</label>
+        <input
+          type="text"
+          class="form-control"
+          id="descripcion"
+          v-model="diagrama.descripcion"
+        />
+      </div>
+    </template>
+
+    <template v-slot:footer>
+      <div class="d-flex align-items-center justify-content-between">
+        <button
+          class="btn btn-danger"
+          @click="this.$refs.modalInternoDiagrama.closeModal()"
+        >
+          Salir
+        </button>
+        <button
+          class="btn btn-secondary"
+          @click="guardarDiagrama"
+          :disabled="enableGuardar"
+        >
+          Guardar
+        </button>
+      </div>
+    </template>
+  </modal-component>
+</template>
+
+<script>
+import ModalComponent from "../../../assets/ModalComponent.vue";
+import { db } from "../../../../firebase/db";
+
+export default {
+  components: { ModalComponent },
+  props: ["diagrama", "isUpdate", "diagramas", "proyecto_id"],
+  data() {
+    return {
+      isValidDiagrama: true,
+    };
+  },
+  computed: {
+    enableGuardar() {
+      return !this.isValidDiagrama;
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$refs.modalinternoEspecificacion.closeModal();
+    },
+    openModal() {
+      this.isValidDiagrama = true;
+      this.$refs.modalinternoEspecificacion.openModal();
+    },
+    guardarEspecificacion() {
+      if (this.isValidDiagrama) {
+        this.isValidDiagrama = false;
+        this.isUpdate ? this.updateDiagrama() : this.postDiagrama();
+        this.$refs.modalinternoEspecificacion.closeModal();
+      }
+    },
+    postDiagrama: function () {
+      db.collection("especificaciones")
+        .doc(this.proyecto_id)
+        .collection("diagramas")
+        .add(this.diagrama);
+      /*const response = axios.post(
+        BASE_URL + "/api/proyecto/" + this.proyecto_id + "/actor",
+        this.especificacion
+      );
+      response.then((response) =>  this.especificaciones.push(response.data.data));*/
+    },
+    updateDiagrama: function () {
+      db.collection("especificaciones")
+        .doc(this.proyecto_id)
+        .collection("diagramas")
+        .doc(this.diagrama.id)
+        .update(this.diagrama);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/resources/js/components/pages/project/Diagrama/DiagramaListComponent.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="card">
+    <div class="card-header">
+      Diagrama
+      <button class="btn btn-primary" @click="createDiagrama">crear</button>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li
+        id="DiagramaRowComponent"
+        class="card mb-1 shadow-sm w-100"
+        v-for="(diagrama, index) in diagramas"
+        :key="index"
+      >
+        <div class="card-body">
+          <div class="cart-title">
+            {{  diagrama.type +": "+ diagrama.nombre }}
+            <button
+              type="button"
+              class="close ml-1"
+              aria-label="Close"
+              style="color: #e74c3c"
+              v-on:click="deleteDiagrama(diagrama)"
+            >
+              <font-awesome-icon :icon="['fas', 'times']" />
+            </button>
+            <button
+              id="editDiagrama"
+              type="button"
+              class="close"
+              aria-label="Update"
+              style="color: #e67e22"
+              v-on:click="toggleUpdate(diagrama)"
+            >
+              <font-awesome-icon :icon="['fas', 'edit']" />
+            </button>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <diagrama-modal-component
+      :isUpdate="editMode"
+      :diagrama="diagrama"
+      :diagramas="diagramas"
+      :proyecto_id="proyecto_id"
+      ref="modalDiagrama"
+    />
+
+<diagrama-component>
+
+  </div>
+</template>
+
+
+<script>
+import { db } from "../../../../firebase/db.js"
+import DiagramaModalComponent from "./DiagramaComponent.vue"
+
+export default {
+     components: { DiagramaModalComponent },
+     props: ["proyecto_id"],
+  data() {
+    return {
+      editMode: false,
+      diagrama: {
+        type: "CU",
+        nombre: "",
+        descripcion: "",
+        entidades: {},
+        relaciones: {}
+      },
+      diagramas: [],
+    };
+  },
+  methods: {
+    createDiagrama: function () {
+      this.editMode = false;
+      this.diagrama = {
+        type: "CU",
+        nombre: "",
+        descripcion: "",
+        entidades: {},
+        relaciones: {}
+      };
+      this.$refs.modalDiagrama.openModal();
+    },
+    deleteDiagrama: function (diagrama) {
+      db.collection("especificaciones").doc(""+this.proyecto_id+"").collection("diagramas").doc(diagrama.id).delete();
+    },
+    toggleUpdate: function (diagrama) {
+      this.editMode = true;
+      this.diagrama = diagrama;
+      this.$refs.modalDiagrama.openModal();
+    },
+  },
+  mounted() {
+  },
+  firestore(){
+    return {
+      diagramas: db.collection("especificaciones").doc(""+this.proyecto_id+"").collection("diagramas")
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
