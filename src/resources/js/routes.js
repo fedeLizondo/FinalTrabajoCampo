0daa9d8df96d82496c6d3f38c1a0b8f7ade9160f
Fede Lizondo <fedealiza@gmail.com>
2021-06-03 02:32:59 -0300

    test real time

--- src/resources/js/routes.js
+++ src/resources/js/routes.js
@@ -3,10 +3,12 @@ import Example from './components/Example.vue';
 import Proyecto from './components/pages/project/ProyectoComponent.vue'
 import Search from './components/pages/search/SearchComponent.vue'
 import Main from './components/pages/main/MainComponent.vue'
+import Diagrama from './components/DiagramaClaseComponente.vue'
 
 
 export const routes = [
     { path: '/', component: Home, name: 'Home' },
+    { path: '/test', component: Diagrama, name: 'diagrama' },
     { path: '/example', component: Example, name: 'Example' },
     { path: '/proyecto/:id', component: Proyecto, name: 'Proyecto', params: true },
     { path: '/search', component: Search, name: 'Search', params: true },
