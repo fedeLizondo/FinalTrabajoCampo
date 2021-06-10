599ee666a05361a5669e780613af53e68ae40a68
Fede Lizondo <fedealiza@gmail.com>
2021-06-10 04:41:00 -0300

    test case insensitive

--- src/resources/js/components/pages/main/MainComponent.vue
+++ src/resources/js/components/pages/main/MainComponent.vue
@@ -1,5 +1,42 @@
 <template>
   <div>
+    <div id="accordion-proyectos">
+      <div class="card">
+        <div class="card-header" id="headingOne">
+          <h2 class="mb-0">
+            Proyectos
+            <button
+              class="btn btn-link"
+              data-toggle="collapse"
+              data-target="#accordion-body"
+              aria-expanded="true"
+              aria-controls="accordion-body"
+            >
+              Ocultar
+            </button>
+          </h2>
+        </div>
+
+        <div
+          id="accordion-body"
+          class="collapse show"
+          aria-labelledby="headingOne"
+          data-parent="#accordion-proyectos"
+        >
+          <div class="card-body d-flex flex-wrap justify-content-center">
+            <div v-for="(proyecto, index) in proyectos" :key="index" class="p-2">
+              <div class="card" style="width: 18rem">
+                <div class="card-body">
+                  <h5 class="card-title"> {{proyecto.nombre}} </h5>
+                  <a href="/proyecto/{{proyecto.id}}" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <h2>Listado para Proyectos</h2>
     <ul>
       <li><router-link to="/proyecto/1"> Ir a proyecto 1</router-link></li>
@@ -10,11 +47,11 @@
 
 <script>
 export default {
-   props: ["user_id"],
+  props: ["user_id"],
   data() {
     return {
       proyectos: [],
-      favoritos: []
+      favoritos: [],
     };
   },
   methods: {
@@ -27,7 +64,7 @@ export default {
     },
     getFavoritos: async function () {
       const response = axios.get(
-        BASE_URL + "/api/"+this.user_id +"/favorito" 
+        BASE_URL + "/api/" + this.user_id + "/favorito"
       );
       response.then((res) => (this.favoritos = res.data.data));
     },
@@ -35,8 +72,7 @@ export default {
   mounted() {
     this.getProyectos();
     this.getFavoritos();
-  }
-
+  },
 };
 </script>
 
