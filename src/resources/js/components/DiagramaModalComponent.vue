646d913d199c1cb86dcf3deaeea41bcd4e972b81
Fede Lizondo <fedealiza@gmail.com>
2021-06-17 17:07:58 -0300

    test case insensitive

--- src/resources/js/components/DiagramaModalComponent.vue
+++ src/resources/js/components/DiagramaModalComponent.vue
@@ -23,7 +23,11 @@
         />
       </div>
 
-      <div v-show="diagrama.data != null && diagrama.data.length > 0 && !isSecuencia">
+      <div
+        v-show="
+          diagrama.data != null && diagrama.data.length > 0 && !isSecuencia
+        "
+      >
         <div class="form-group">
           <label for="relacionInput">Relacion:</label>
           <div class="input-group mb-3" id="relacionInput">
@@ -71,11 +75,15 @@
         </div>
 
         <ul class="list-group">
-          <li class="list-group-item" v-for="(value,index) in diagrama.data.next" :key="index">
-              {{ diagrama.data.link[index]}} {{value}}
-              <button class="btn btn-danger"> 
-                <font-awesome-icon :icon="['fas', 'trash']" />
-              </button>
+          <li
+            class="list-group-item"
+            v-for="(value, index) in elemento.next"
+            :key="index"
+          >
+            {{ elemento.link[index] }} {{ value }}
+            <button class="btn btn-danger">
+              <font-awesome-icon :icon="['fas', 'trash']" />
+            </button>
           </li>
         </ul>
       </div>
@@ -83,12 +91,7 @@
 
     <template v-slot:footer>
       <div class="d-flex align-items-center justify-content-between">
-        <button
-          class="btn btn-danger"
-          @click="closeModal"
-        >
-          Salir
-        </button>
+        <button class="btn btn-danger" @click="closeModal">Salir</button>
         <button
           class="btn btn-secondary"
           @click="guardarDiagrama"
@@ -112,8 +115,8 @@ export default {
   data() {
     return {
       isValidDiagrama: true,
-      to:"",
-      typeRelation:"",
+      to: "",
+      typeRelation: "",
     };
   },
   computed: {
@@ -126,20 +129,33 @@ export default {
     isCU() {
       return this.diagrama.type == "CU";
     },
-    isClase(){
+    isClase() {
       return this.diagrama.type == "CLASE";
     },
-    isSecuencia(){
+    isSecuencia() {
       return this.diagrama.type == "SECUENCIA";
     },
     relationTypes() {
       switch (this.diagrama.type) {
         case "CU":
-          return [{name:"use",value:"--use--"},{name:"include",value:"-.include.->"},{name:"extend",value:"--extend-->"}];
+          return [
+            { name: "use", value: "--use--" },
+            { name: "include", value: "-.include.->" },
+            { name: "extend", value: "--extend-->" },
+          ];
         case "CLASE":
-          return [{name:"Herencia",value:"<|--"},{name:"Composición", value:"*--"},{name:"Agregación",value:"o--"},{name:"Asociación",value:"--"},{name:"Dependencia",value:"..>"}]
+          return [
+            { name: "Herencia", value: "<|--" },
+            { name: "Composición", value: "*--" },
+            { name: "Agregación", value: "o--" },
+            { name: "Asociación", value: "--" },
+            { name: "Dependencia", value: "..>" },
+          ];
         case "SECUENCIA":
-          return [{name:"-->",value:"->>"},{name:"<--",value:"-->>"}];
+          return [
+            { name: "-->", value: "->>" },
+            { name: "<--", value: "-->>" },
+          ];
         default:
           return [];
       }
@@ -161,11 +177,11 @@ export default {
       }
     },
     postDiagrama: function () {
-        if(this.isCU && this.elemento.id == ""){
-          this.elemento.id = Date.now();
-        }
+      if (this.isCU && this.elemento.id == "") {
+        this.elemento.id = Date.now();
+      }
 
-      this.diagrama.data.push({...this.elemento});
+      this.diagrama.data.push({ ...this.elemento });
 
       db.collection("especificaciones")
         .doc(this.proyecto_id)
@@ -173,7 +189,7 @@ export default {
         .doc(this.diagrama.id)
         .update(this.diagrama);
 
-      this.closeModal();  
+      this.closeModal();
     },
     updateDiagrama: function () {
       /* db.collection("especificaciones")
@@ -185,9 +201,9 @@ export default {
     addRelation: function () {
       this.elemento.next.push[this.to];
       this.elemento.link.push[this.relationTypes];
-      this.to="";
-      this.typeRelation="";
-    }
+      this.to = "";
+      this.typeRelation = "";
+    },
   },
 };
 </script>
